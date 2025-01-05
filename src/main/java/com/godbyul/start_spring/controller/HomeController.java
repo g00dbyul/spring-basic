bfcd3c65546e33c62f90bc5833a4eb4fcc36055d
g00dbyul <skypower4203@gmail.com>
2025-01-05 23:06:55 +0900

    set member front end template

---
+++ src/main/java/com/godbyul/start_spring/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.godbyul.start_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
